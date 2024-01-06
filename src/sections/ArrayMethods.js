dd571f0fb7887bf99c3fbca986cb71528c51586a
randhavevaibhav <randhavevaibhav3@gmail.com>
2024-01-06 21:10:45 +0530

    Abstracted code from Array and String topics

--- src/sections/ArrayMethods.js
+++ src/sections/ArrayMethods.js
@@ -1,56 +1,10 @@
-import {React,useEffect,useState} from "react";
-import axios from "axios";
-import TopicItems from "../components/TopicItems";
+import React from "react";
+import CreateSection from "./CreateSection";
 
-import { LoadingSkeleton } from '../helper/LoadingHelper';
-import "../styles/SkeletonLoading.css";
 function ArrayMethods() {
-
-const QTopicName = "Array";
-const [fetchTopic,setFetchTopic] = useState([]);
-const [loading,setLoading] =  useState(false);
-
-
-useEffect(()=>{
-  setLoading(true);
-  
-  axios.get(`https://js-manual2-backend.vercel.app/jstopics/${QTopicName}`)
-  .then((res)=>{
-    setLoading(false);
-   
-    setFetchTopic(res.data.data) ;
-  })
-  
-  .catch((err)=>{
-    console.log(err);
-  })
-  
-  
-
-
-},[])
   return (
     <>
-    <div className='main-container'>
-
-    {loading ? <LoadingSkeleton skeletonItems={5}/>:fetchTopic.map((topic, id) => {
-        return (
-          <TopicItems
-            key={id}
-            topicName={topic.subTopicName}
-            syntax={topic.syntax}
-            explanation={topic.explanation}
-            IMPPoints={topic.IMPPoints}
-            videoLink={topic.videoLink}
-            videoTitle={topic.videoTitle}
-          ></TopicItems>
-        );
-      })}
-
-    </div>
-
-   
-     
+      <CreateSection QTopicName={"Array"} />
     </>
   );
 }
